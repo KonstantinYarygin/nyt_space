b6b3b020707dbffd7af0f35750d7845dedaa929b
KonstantinYarygin <yarygin@phystech.edu>
2017-04-21 21:00:13 +0300

    plot cosine sim plot

--- plot.R
+++ plot.R
@@ -2,21 +2,26 @@ setwd('/home/konstantin/documents/projects/nyt_space/')
 rm(list=ls())
 dev.off()
 
-df <- read.csv('nasa_counts.csv', header=F)
-colnames(df) <- c('year', 'n_all', 'n_nasa')
-df$n_not_nasa <- df$n_all - df$n_nasa
-df <- df[order(df$year), ]
-rownames(df) <- df$year
-head(df)
+library(gplots)
+tp_col <- function(col, alpha=0.5) {
+    hex_alpha <- as.hexmode(floor(alpha * 255))
+    return(paste0(col2hex(col), hex_alpha))
+}
 
-pdf('NASA_mentions.pdf', 9, 6)
-counts <- table(df$year, df$n_nasa, df$n_not_nasa)
-barplot(t(as.matrix(df[, c('n_nasa', 'n_not_nasa')])),
-        axes=F, col=c('gray40', 'gray70'))
-axis(side=2, labels=seq(0, 500, 100), at=seq(0, 500, 100), lwd=2, las=1)
+df <- read.csv('cos_sim.csv')
 
-legend('topleft', c('NASA not mentioned', 'NASA mentioned'),
-       col=c('gray70', 'gray40'),
-       pch=15, pt.cex=1, # points param
+plot(NULL, axes=F,
+     xlim=c(2003, 2016), ylim=c(0.4, 0.9),
+     xlab='', ylab='Similarity', main='')
+abline(v=seq(2003, 2016, 1), h=seq(0.4, 0.9, 0.05),
+       col='gray80', lwd=1)
+lines(similarity~year, df[df$type == 'yyu',], col='red', lwd=2, lty=1)
+lines(similarity~year, df[df$type == 'yhu',], col='royalblue', lwd=2, lty=1)
+lines(similarity~year, df[df$type == 'yyi',], col='green', lwd=2, lty=1)
+lines(similarity~year, df[df$type == 'yhi',], col='black', lwd=2, lty=1)
+axis(side=1, labels=seq(2003, 2016, 1), at=seq(2003, 2016, 1), lwd=2, las=1)
+axis(side=2, labels=seq(0.4, 0.9, 0.05), at=seq(0.4, 0.9, 0.05), lwd=2, las=1)
+legend('bottomright', c('year-year, union', 'year-heap, union', 'year-year, intersect', 'year-heap, intersect'),
+       col=c('red', 'royalblue', 'green', 'black'),
+       lwd=2,
        bty='n')
-dev.off()
